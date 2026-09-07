3e57022c36f75b76f2b187f36d86684e1d36ce8f
Jussi Nevavuori <jussi@nevavuori.fi>
2026-09-07 19:13:34 +0300

    feat: work on game

--- src/features/game/hooks/useGameForm.ts
+++ src/features/game/hooks/useGameForm.ts
@@ -1,12 +1,19 @@
 import { useCallback, useState } from "react";
 import { useWrapRef } from "#/hooks/useWrapRef";
-import { Game, type GameAnswerMode } from "../utils/game";
+import type { GameAnswerMode } from "../utils/answerMode";
+import { Game } from "../utils/game";
 
+/**
+ * Hook for instantiating a new game.
+ */
 export function useGameForm(args: { onGameStart: (game: Game) => void }) {
+	// Wrap in a stable ref for callbacks
 	const onGameStartRef = useWrapRef(args.onGameStart);
 
+	// Selected answer mode
 	const [answerMode, setAnswerMode] = useState<GameAnswerMode>("select");
 
+	// Start the game: Create Game instance from settings and invoke callback
 	const startGame = useCallback(() => {
 		const game = new Game({ answerMode });
 		onGameStartRef.current(game);
